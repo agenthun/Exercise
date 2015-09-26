d235283c021b5bf540211d2a2ec2963be19614f0
agenthun <hun333@126.com>
2015-09-26 13:54:27 +0800

    Day 14 if负数比较

---
+++ src/com/agenthun/ex/CompareNegative.java
@@ -0,0 +1,12 @@
+package com.agenthun.ex;
+
+/**
+ * Created by agenthun on 15/9/26.
+ */
+public class CompareNegative {
+    public static void main(String[] args) {
+        int k = -3;
+        if (k <= 0) System.out.println("k<=0");
+        else System.out.println("k>0");
+    }
+}
